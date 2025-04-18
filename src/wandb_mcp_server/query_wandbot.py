75f135c57c4e7859d988716c8307f4132af35d34
Morgan McGuire <morganmcg1@users.noreply.github.com>
2025-04-18 19:42:42 +0100

    Update from mcp_server to wandb_mcp_server

---
+++ src/wandb_mcp_server/query_wandbot.py
@@ -0,0 +1,7 @@
+"""
+Import the query_wandbot_api function from the wandbot package, a helper function
+to query the Weights & Biases support bot.
+"""
+
+from wandbot.wandbot_tool import query_wandbot_api, WANDBOT_TOOL_DESCRIPTION
+
